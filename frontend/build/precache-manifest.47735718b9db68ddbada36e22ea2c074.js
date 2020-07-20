29602c0735f1a6c44eeba39fd566a856e191a19b
Mohamed Tarek <55949665+Mohamedtarek22@users.noreply.github.com>
2020-07-20 21:28:17 +0200

    Add files via upload

---
+++ frontend/build/precache-manifest.47735718b9db68ddbada36e22ea2c074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e66f8a31d6d41252b0d97441542de8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "017fa40f9d70a48b8998",
+    "url": "/static/css/main.8fba3394.chunk.css"
+  },
+  {
+    "revision": "d044d89feee757a4a31a",
+    "url": "/static/js/2.9f263521.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.9f263521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017fa40f9d70a48b8998",
+    "url": "/static/js/main.b0ab8f08.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ frontend/src/data.js
@@ -0,0 +1,62 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "Slim shirt",
+      image: "/images/Arthur-Kulkov-American-GQ-Style-008.jpg",
+      price: 60,
+      category: "Shirts",
+      brand: "LC",
+      rating: 5,
+      numReviews: 10,
+      countInStock : 6
+    },
+    {
+      _id: "2",
+      name: "Slim shirt",
+      category: "Pants",
+      image: "/images/Arthur-Kulkov-American-GQ-Style-008.jpg",
+      price: 60,
+      brand: "LC",
+      rating: 5,
+      numReviews: 10,
+      countInStock : 6
+    },
+    {
+      _id: "3",
+      name: "Slim shirt",
+      image: "/images/Arthur-Kulkov-American-GQ-Style-008.jpg",
+      price: 60,
+      category: "Pants",
+
+      brand: "LC",
+      rating: 5,
+      numReviews: 10,
+      countInStock : 6
+    },
+    {
+      _id: "4",
+      name: "Slim shirt",
+      image: "/images/Arthur-Kulkov-American-GQ-Style-008.jpg",
+      price: 60,
+      brand: "LC",
+      category: "Pants",
+      rating: 5,
+      numReviews: 10,
+      countInStock : 6
+    },
+    {
+      _id: "5",
+
+      name: "Slim shirt",
+      image: "/images/Arthur-Kulkov-American-GQ-Style-008.jpg",
+      price: 60,
+      brand: "LC",
+      category: "Pants",
+
+      rating: 5,
+      numReviews: 10,
+      countInStock : 6
+    },
+  ],
+};
